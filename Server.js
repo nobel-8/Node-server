95542096ddd4fac2ce32f9d945700d418449786b
Nair <724446@cognizant.com>
2018-10-22 17:25:24 +0530

    done for today Monday

--- Server.js
+++ Server.js
@@ -1,6 +1,10 @@
 var http = require('http');
 
 http.createServer(function (req, res) {
+    if (err) throw err;
+   console.log(err)
     res.writeHead(200, {'Content-Type': 'text/html'});
+   // res.write('Hello World');
     res.end('Hello World!');
-}).listen(8080);
+
+}).listen(8000);
